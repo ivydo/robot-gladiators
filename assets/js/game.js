923f35e97a14de468c7e44db18b0a22534229ba4
Ivy Do <ivy.do2011@gmail.com>
2021-11-02 20:30:13 -0400

    var playerName

---
+++ assets/js/game.js
@@ -0,0 +1,12 @@
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+window.alert(playerName);
+
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
