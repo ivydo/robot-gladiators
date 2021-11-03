2fef7ca9c05a32e9ad7f06dfe4b24c0b3259cded
Ivy Do <ivy.do2011@gmail.com>
2021-11-02 20:37:20 -0400

    created var for game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,16 @@
 var playerName = window.prompt("What is your robot's name?");
-// Note the lack of quotation marks around playerName
-window.alert(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-// this creates a function named "fight"
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-function fight() {
-    window.alert("The fight has begun!");
-}
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+};
 
-// fight();
+fight();
